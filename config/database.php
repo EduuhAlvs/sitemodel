3633ad3e698862476a2abbdec4ad8fc89458b02d
EduuhAlvs <33768014+EduuhAlvs@users.noreply.github.com>
2026-02-07 07:18:39 -0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -3,13 +3,14 @@
 
 return [
     'host' => 'localhost',
-    'dbname' => 'u669181089_topmodel',
-    'user' => 'u669181089_topmodel',      // Altere para seu usuário
-    'password' => 'Tp102030=',      // Altere para sua senha
+    'dbname' => '_topmodel',
+    'user' => '_topmodel',      // Altere para seu usuário
+    'password' => '123456',      // Altere para sua senha
     'charset' => 'utf8mb4',
     'options' => [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES => false,
     ]
+
 ];
